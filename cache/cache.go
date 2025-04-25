73bece524e62607d1db382f63e43a47883c371bf
tik676 <tik_676@mail.ru>
2025-04-25 10:20:14 +0500

    Initial commit

---
+++ cache/cache.go
@@ -0,0 +1,57 @@
+package cache
+
+import (
+	"sync"
+	"time"
+)
+
+type T interface{}
+
+type item struct {
+	value   T
+	expires time.Time
+}
+
+type Cache struct {
+	mu    sync.RWMutex
+	items map[string]item
+	ttl   time.Duration
+}
+
+func (c *Cache) Set(key string, value T) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.items[key] = item{
+		value:   value,
+		expires: time.Now().Add(c.ttl),
+	}
+}
+
+func (c *Cache) Get(key string) (T, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	v, ok := c.items[key]
+	if ok {
+		return v, true
+	}
+	for k, v := range c.items {
+		if time.Now().After(v.expires) {
+			delete(c.items, k)
+		}
+	}
+	return nil, false
+}
+
+func (c *Cache) Clear() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.items = make(map[string]item)
+}
+
+func (c *Cache) Delete(key string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	delete(c.items, key)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tik676/inmemory-cache
+
+go 1.24.2
